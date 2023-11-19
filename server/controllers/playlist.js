04b26a1bf9e9ddbee4b8e63948672def544f2954
Quynh Nguyen <qtn2@case.edu>
2023-11-19 10:57:49 -0500

    playlist and user controller

---
+++ server/controllers/playlist.js
@@ -0,0 +1,9 @@
+import { pool } from '../config/database.js'
+
+const createPlaylist = async (req, res) => {
+    try {
+
+    } catch (err) {[
+        res.status(409).json({ message: err.message })
+    ]}
+}
